7671bf4e1e2fa14f7bb4379c29ae0a7c462d492b
Phillip Verheyden <pverheyden@gmail.com>
2018-01-19 16:17:21 -0600

    GCE support and default to paste mode in vim

---
+++ system/gcloud.zsh
@@ -0,0 +1,9 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f /Users/phillip/google-cloud-sdk/path.zsh.inc ]; then
+  source '/Users/phillip/google-cloud-sdk/path.zsh.inc'
+fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f /Users/phillip/google-cloud-sdk/completion.zsh.inc ]; then
+  source '/Users/phillip/google-cloud-sdk/completion.zsh.inc'
+fi
