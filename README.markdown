bd0744a2d0ca713ed44e4dbc52a4ede365704e84
Phillip Verheyden <pverheyden@gmail.com>
2025-04-17 13:31:14 -0500

    Fix gnu paths to use Homebrew arm

--- README.markdown
+++ README.markdown
@@ -10,7 +10,7 @@ configuration by myself, so this no longer requires that.
 - Install [iTerm](https://iterm2.com/)
 - Install [Homebrew](https://brew.sh/)
 - Install a JVM from https://adoptopenjdk.net/ (last 3 LTS versions)
-- `brew install autojump autoconf awscli bzip2 ca-certificates cffi cmake coreutils curl gcc gettext git git-lfs glib gnu-time grc grep hub jq libgpg-error libtool libuv lz4 mongosh ncdu nvm pinentry pinentry-mac poetry pyenv rbenv ruby-build rsync tfenv tree uv vim wget yq zsh`
+- `brew install autojump autoconf awscli bzip2 ca-certificates cffi cmake coreutils curl gcc gettext git git-lfs glib gnu-sed gnu-time grc grep hub jq libgpg-error libtool libuv lz4 mongosh ncdu nvm pinentry pinentry-mac poetry pyenv rbenv ruby-build rsync tfenv tree uv vim wget yq zsh`
 - Install sdkman `brew tap sdkman/tap && brew install sdkman-cli && sdk install java`
 - `chsh -s /bin/zsh`
 - Copy SSH key from Bitwarden to ~/.ssh/id_rsa and set correct chmod permissions `chmod 600 ~/.ssh/id_rsa`
--- system/env.zsh
+++ system/env.zsh
@@ -14,8 +14,8 @@ export NODE_PATH="/usr/local/lib/node"
 export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules
 export NODE_OPTIONS="--max-old-space-size=8192"
 export GOPATH=$HOME/.go
-export MANPATH="/opt/local/share/main:/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/usr/local/opt/coreutils/libexec/gnuman:/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
-export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/findutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:$ZSH/bin:$ZSH/bin/private:/usr/local/bin:/usr/local/sbin:storm:/usr/local/Cellar/go/1.2.1/libexec/bin:/usr/local/mysql/bin:$HOME/.cargo/bin:$HOME/.local/bin:/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+export MANPATH="/opt/local/share/main:/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:/opt/homebrew/opt/coreutils/libexec/gnuman:/opt/homebrew/opt/gnu-sed/libexec/gnuman:$MANPATH"
+export PATH="/opt/local/bin:/opt/local/sbin:/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/findutils/libexec/gnubin:/opt/homebrew/opt/gnu-sed/libexec/gnubin:$ZSH/bin:$ZSH/bin/private:/usr/local/bin:/usr/local/sbin:storm:/opt/homebrew/opt/go/1.2.1/libexec/bin:/usr/local/mysql/bin:$HOME/.cargo/bin:$HOME/.local/bin:/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
 
 # Environment settings
 setopt NO_BG_NICE # don't nice background tasks
