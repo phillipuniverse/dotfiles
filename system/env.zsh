72b18df694a0ca99d18052f975418451d8baf363
Phillip Verheyden <pverheyden@gmail.com>
2026-05-21 10:14:25 -0500

    byebye pyenv

--- system/env.zsh
+++ system/env.zsh
@@ -81,12 +81,6 @@ export GUILE_LOAD_COMPILED_PATH="/usr/local/lib/guile/3.0/site-ccache"
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-# Initializes pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-
 # Initialize rbenv
 eval "$(rbenv init - zsh)"
 
